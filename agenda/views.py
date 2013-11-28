428bb178bf781525b3a85b80cd7d2324c62c71be
Marco <marco.istccp@gmail.com>
2013-11-27 22:20:41 -0200

    add app agenda

---
+++ agenda/views.py
@@ -0,0 +1,10 @@
+#encoding: utf-8
+
+from django.shortcuts import render_to_response
+
+from models import ItemAgenda
+
+def lista(request):
+	lista_itens = ItemAgenda.objects.all()
+	return render_to_response("lista.html",
+	 	{'lista_itens': lista_itens})
