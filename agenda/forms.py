6a263375bae9d76a24cf86d418b0a0378b027f63
Marco <marco.istccp@gmail.com>
2013-12-20 06:54:56 -0200

    faltava forms.py

---
+++ agenda/forms.py
@@ -0,0 +1,13 @@
+#encoding: utf-8
+
+from django.forms import ModelForm
+from agenda.models import ItemAgenda
+
+class FormItemAgenda(forms.ModelForm):
+	data = forms.DateField(
+					widget=forms.DateInput(format="%d/%m/%Y"),
+					input_formats=['%d/%m/%Y', '%d/%m/%y'])
+
+	class Meta:
+		model = ItemAgenda
+		fields = ('titulo', 'data', 'hora', 'descricao')
