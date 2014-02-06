cd69bfa169756e8f029a8314e62720dfec1ace7b
Marco <marco.istccp@gmail.com>
2014-02-06 09:02:08 -0200

    correção na validação de usuários logados

--- agenda/views.py
+++ agenda/views.py
@@ -1,35 +1,60 @@
 #encoding: utf-8
 
-from django.shortcuts import render_to_response
-
+from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
-
-from models import ItemAgenda
-
-from forms import FormItemAgenda
-
-from django.shortcuts import get_object_or_404
-
 from django.contrib.auth.decorators import login_required
 
+from models import ItemAgenda
+from forms import FormItemAgenda
+
+#from django.shortcuts import
+
+#def lista(request):
+#	lista_itens = ItemAgenda.objects.all()
+#	return render_to_response("lista.html", {'lista_itens': lista_itens})
+
+#def adiciona(request):
+#	if request.method == 'POST': 
+#		form = FormItemAgenda(request.POST, request.FILES)
+#		if form.is_valid():
+#			form.save()
+#			return render_to_response("salvo.html", {})	
+
+#	else:
+#		form = FormItemAgenda()
+#	return render_to_response("item.html", {'form': form},
+#	context_instance=RequestContext(request))		
+
+#def item(request, nr_item):
+#	item = get_object_or_404(ItemAgenda, pk=nr_item) 
+#	if request.method == 'POST':
+#		form = FormItemAgenda(request.POST, request.FILES, instance=item)
+#		if form.is_valid():
+#			form.save()
+#			return render_to_response("salvo.html", {})
+#	else:
+#		form = FormItemAgenda(instance=item)		
+#	return render_to_response("item.html", {'form': form}, context_instance=RequestContext(request))
+
+@login_required
 def lista(request):
-	lista_itens = ItemAgenda.objects.all()
-	return render_to_response("lista.html", {'lista_itens': lista_itens})
+	lista_itens = ItemAgenda.objects.filter(usuario=request.user)
+	return render_to_response("lista.html", {'lista_itens': lista_itens},
+		context_instance=RequestContext(request))
 
+@login_required
 def adiciona(request):
-	if request.method == 'POST': 
-		form = FormItemAgenda(request.POST, request.FILES)
-		if form.is_valid():
-			form.save()
-			return render_to_response("salvo.html", {})	
+    if request.method == "POST":
+    	form = FormItemAgenda(request.POST, request.FILES)
+    	if form.is_valid():
+			item = form.save(commit=False)
+			item.usuario = request.user
+			item.save()
+			return render_to_response("salvo.html", {})
 
-	else:
-		form = FormItemAgenda()
-	return render_to_response("item.html", {'form': form},
-	context_instance=RequestContext(request))		
-
+@login_required
 def item(request, nr_item):
-	item = get_object_or_404(ItemAgenda, pk=nr_item) 
+	item = get_object_or_404(ItemAgenda, pk=nr_item, usuario=request.user)
 	if request.method == 'POST':
 		form = FormItemAgenda(request.POST, request.FILES, instance=item)
 		if form.is_valid():
@@ -37,21 +62,5 @@ def item(request, nr_item):
 			return render_to_response("salvo.html", {})
 	else:
 		form = FormItemAgenda(instance=item)		
-	return render_to_response("item.html", {'form': form}, context_instance=RequestContext(request))
-
-@login_required
-def lista(request):
-	lista_itens = ItemAgenda.objects.filter(usuario=request.user)
-	return render_to_response("lista.html", {'lista_itens': lista_itens})
-
-@login_required
-def adiciona(request):
-	if form.is_valid():
-		item = form.save(commit=False)
-		item.usuario = request.user
-		item.save()
-
-@login_required
-def item(request, nr_item):
-	item = get_object_or_404(ItemAgenda, pk=nr_item, usuario=request.user)
-	if request.method == 'POST':
+	return render_to_response("item.html", {'form': form},
+		context_instance=RequestContext(request))	
